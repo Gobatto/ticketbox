e21a9fb174268f29fea7aac3b89f58487f759db0
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-08-03 11:36:21 -0300

    updating mustash

--- app.js
+++ app.js
@@ -9,5 +9,27 @@ const app = new Vue({
       referrals: [],
       specialRequests: '',
       purchaseAgreementSigned: false
+    },
+    computed: {
+      fullName: function() {
+        if (this.firstName && this.lastName) {
+          return this.firstName + ' ' + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      ticketDescription: function() {
+        let readableTicketType = 'General Admission';
+        if (this.ticketType === 'vip') {
+          readableTicketType = 'VIP';
+        }
+  
+        let ticketPluralization = 'tickets';
+        if (this.ticketQuantity === 1) {
+          ticketPluralization = 'ticket';
+        }
+  
+        return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
+      }
     }
   });
